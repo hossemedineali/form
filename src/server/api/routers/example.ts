c3b44be93eba07ba438e79462d4a0b45dc6ee8bb
hossemedineali <hossemedine@outlook.com>
2023-03-27 22:09:05 +0100

    first

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const exampleRouter = createTRPCRouter({
+  save: publicProcedure
+    .input(z.object({ name:z.string(),email:z.string(),phone:z.string().optional(), }))
+    .mutation(({ input,ctx }) => {
+      
+      return ctx.prisma.form.create({
+        data:{...input}
+      })
+    }),
+  load: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.form.findFirst({
+      select:{
+        id:false,
+        name:true,
+        email:true,
+        phone:true
+      }
+    })
+  }),
+});
